3a3828131255a9064843deb3643a6513f6c9f9d1
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-06-26 15:14:49 +0300

    .

--- src/main/java/com/tugalsan/api/file/img/code/server/TS_FileImageCodeQRUtils.java
+++ src/main/java/com/tugalsan/api/file/img/code/server/TS_FileImageCodeQRUtils.java
@@ -1,61 +1,61 @@
 package com.tugalsan.api.file.img.code.server;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.tugalsan.api.function.client.maythrowexceptions.checked.TGS_FuncMTCUtils;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 public class TS_FileImageCodeQRUtils {
 
-    public static BufferedImage createQR(String barcodeText) throws Exception {
-        var barcodeWriter = new QRCodeWriter();
-        var matrix = barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
-        return MatrixToImageWriter.toBufferedImage(matrix);
+    public static BitMatrix toMatrix(String barcodeText) {
+        return TGS_FuncMTCUtils.call(() -> {
+            return new QRCodeWriter().encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
+        });
     }
 
-    public static BufferedImage createQRwithText(String barcodeText, String topText, String bottomText) throws WriterException, IOException {
-        var barcodeWriter = new QRCodeWriter();
-        var matrix = barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200);
-        return modifiedQRCode(matrix, topText, bottomText);
+    public static BufferedImage toQR(String barcodeText) {
+        var qrMatrix = toMatrix(barcodeText);
+        return MatrixToImageWriter.toBufferedImage(qrMatrix);
     }
 
-    public static BufferedImage modifiedQRCode(BitMatrix matrix, String topText, String bottomText) {
+    public static BufferedImage toQRwithLabels(String barcodeText, String lblTop, String lblBottom) {
+        var qrMatrix = toMatrix(barcodeText);
+        var qrWidth = qrMatrix.getWidth();
+        var qrHeight = qrMatrix.getHeight();
+
         //Here, we create a BufferedImage instance that temporarily holds the QR code by drawing its pixels on a Graphics2D object.
-        var matrixWidth = matrix.getWidth();
-        var matrixHeight = matrix.getHeight();
-        var image = new BufferedImage(matrixWidth, matrixHeight, BufferedImage.TYPE_INT_RGB);
-        var graphics = image.createGraphics();
-        graphics.setColor(Color.WHITE);
-        graphics.fillRect(0, 0, matrixWidth, matrixHeight);
-        graphics.setColor(Color.BLACK);
-        for (var i = 0; i < matrixWidth; i++) {
-            for (var j = 0; j < matrixHeight; j++) {
-                if (matrix.get(i, j)) {
-                    graphics.fillRect(i, j, 1, 1);
+        var qrImage = new BufferedImage(qrWidth, qrHeight, BufferedImage.TYPE_INT_RGB);
+        var qrImageGraphics = qrImage.createGraphics();
+        qrImageGraphics.setColor(Color.WHITE);
+        qrImageGraphics.fillRect(0, 0, qrWidth, qrHeight);
+        qrImageGraphics.setColor(Color.BLACK);
+        for (var i = 0; i < qrWidth; i++) {
+            for (var j = 0; j < qrHeight; j++) {
+                if (qrMatrix.get(i, j)) {
+                    qrImageGraphics.fillRect(i, j, 1, 1);
                 }
             }
         }
         //Next, let’s calculate the dimensions required to accommodate the text and QR code:
-        var fontMetrics = graphics.getFontMetrics();
-        int topTextWidth = fontMetrics.stringWidth(topText);
-        int bottomTextWidth = fontMetrics.stringWidth(bottomText);
-        int finalWidth = Math.max(matrixWidth, Math.max(topTextWidth, bottomTextWidth)) + 1;
-        int finalHeight = matrixHeight + fontMetrics.getHeight() + fontMetrics.getAscent() + 1;
+        var fontMetrics = qrImageGraphics.getFontMetrics();
+        int textWidthTop = fontMetrics.stringWidth(lblTop);
+        int textWidthBottom = fontMetrics.stringWidth(lblBottom);
 
         //Finally, let’s create a new BufferedImage instance to hold the final image:
-        var finalImage = new BufferedImage(finalWidth, finalHeight, BufferedImage.TYPE_INT_RGB);
-        var finalGraphics = finalImage.createGraphics();
-        finalGraphics.setColor(Color.WHITE);
-        finalGraphics.fillRect(0, 0, finalWidth, finalHeight);
-        finalGraphics.setColor(Color.BLACK);
-        finalGraphics.drawImage(image, (finalWidth - matrixWidth) / 2, fontMetrics.getAscent() + 2, null);
-        finalGraphics.drawString(topText, (finalWidth - topTextWidth) / 2, fontMetrics.getAscent() + 2);
-        finalGraphics.drawString(bottomText, (finalWidth - bottomTextWidth) / 2, finalHeight - fontMetrics.getDescent() - 5);
+        int finWidth = Math.max(qrWidth, Math.max(textWidthTop, textWidthBottom)) + 1;
+        int finHeight = qrHeight + fontMetrics.getHeight() + fontMetrics.getAscent() + 1;
+        var finImage = new BufferedImage(finWidth, finHeight, BufferedImage.TYPE_INT_RGB);
+        var finImageGraphics = finImage.createGraphics();
+        finImageGraphics.setColor(Color.WHITE);
+        finImageGraphics.fillRect(0, 0, finWidth, finHeight);
+        finImageGraphics.setColor(Color.BLACK);
+        finImageGraphics.drawImage(qrImage, (finWidth - qrWidth) / 2, fontMetrics.getAscent() + 2, null);
+        finImageGraphics.drawString(lblTop, (finWidth - textWidthTop) / 2, fontMetrics.getAscent() + 2);
+        finImageGraphics.drawString(lblBottom, (finWidth - textWidthBottom) / 2, finHeight - fontMetrics.getDescent() - 5);
 
-        return finalImage;
+        return finImage;
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,6 @@ module com.tugalsan.api.file.img.code {
     requires com.google.zxing;
     requires com.google.zxing.javase;
     requires java.desktop;
+    requires com.tugalsan.api.function;
     exports com.tugalsan.api.file.img.code.server;
 }

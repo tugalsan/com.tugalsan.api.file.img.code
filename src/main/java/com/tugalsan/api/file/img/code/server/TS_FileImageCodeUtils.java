34f2ad5084385664c5083338de71fdcddcd4de04
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-06-26 14:17:56 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/api/file/img/code/server/TS_FileImageCodeUtils.java
@@ -0,0 +1,8 @@
+package com.tugalsan.api.file.img.code.server;
+
+public class TS_FileImageCodeUtils {
+
+    final private static TS_Log d = TS_Log.of(TS_FileImageCodeUtils.class);
+
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.tugalsan.api.file.img.code {
+    requires com.tugalsan.api.log;
+    exports com.tugalsan.api.file.img.code.server;
+}
